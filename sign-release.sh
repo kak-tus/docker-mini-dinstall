c6ad96b03df4cbe25a1d58156055d59dd58017db
Andrey Kuzmin <prepodam@mail.ru>
2018-12-09 00:13:41 +0300

    Sign fix.

--- sign-release.sh
+++ sign-release.sh
@@ -7,7 +7,7 @@ PASSPHRASE=$(cat "/etc/passphrase")
 gpg --help 1>/dev/null 2>&1 || true
 
 rm -f Release.gpg.tmp InRelease.tmp
-echo "$PASSPHRASE" | gpg --no-tty --batch --passphrase-fd=0 --detach-sign -o Release.gpg.tmp "$1"
+echo "$PASSPHRASE" | gpg --no-tty --batch --passphrase-fd=0 -abs -o Release.gpg.tmp "$1"
 mv Release.gpg.tmp Release.gpg
-echo "$PASSPHRASE" | gpg --no-tty --batch --passphrase-fd=0 --clearsign -o InRelease.tmp "$1"
+echo "$PASSPHRASE" | gpg --no-tty --batch --passphrase-fd=0 --clearsign --digest-algo SHA512 -o InRelease.tmp "$1"
 mv InRelease.tmp InRelease
